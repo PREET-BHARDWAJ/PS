370e375de6cc1e369a24be681b15b57a24a3c1f6
PREET-BHARDWAJ <146235081+PREET-BHARDWAJ@users.noreply.github.com>
2023-11-22 12:11:23 +0530

    Add files via upload

---
+++ 2.largest among 3 number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int n1,n2,n3;
+printf("enter n1,n2,n3 : ");
+scanf("%d%d%d",&n1,&n2,&n3);
+if(n1>n2 && n1>n3)
+    printf("n1=%d is largest",n1);
+else if(n2>n3)
+    printf("n2=%d is largest",n2);
+else
+    printf("n3=%d is largest",n3);
+return 0;
+}
